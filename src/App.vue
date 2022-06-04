7fdcebf12cfcc0e909c852690f320926de28dbe6
Joaquin Nuñez <magnanimoysimple@gmail.com>
2022-06-03 21:40:03 -0300

    Primer maqueta

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="header-nav">
+    <router-link to="/"><h3>Conocimientos</h3></router-link>
+    <router-link to="/about"><h3>Proyectos</h3></router-link>
+    <router-link to="/about"><h3>Sobre mi</h3></router-link>
   </nav>
   <router-view />
 </template>
 
 <style>
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+body {
+  background-color: #558595;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 200 200'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='100' y1='33' x2='100' y2='-3'%3E%3Cstop offset='0' stop-color='%23000' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23000' stop-opacity='1'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='100' y1='135' x2='100' y2='97'%3E%3Cstop offset='0' stop-color='%23000' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23000' stop-opacity='1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23417180' fill-opacity='0.6'%3E%3Crect x='100' width='100' height='100'/%3E%3Crect y='100' width='100' height='100'/%3E%3C/g%3E%3Cg fill-opacity='0.5'%3E%3Cpolygon fill='url(%23a)' points='100 30 0 0 200 0'/%3E%3Cpolygon fill='url(%23b)' points='100 100 0 130 0 100 200 100 200 130'/%3E%3C/g%3E%3C/svg%3E");
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #2e2f30;
+  border: 1px solid #aaa;
+  width: 90vw;
+  height: 80vh;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(74, 74, 74, 0.37);
+  backdrop-filter: blur(2px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  display: flex;
+  flex-flow: column nowrap;
 }
 
-nav {
-  padding: 30px;
-}
+.header-nav {
+  width: 90vw;
+  height: 90px;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+.header-nav a {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  border-bottom: 1px solid black;
+}
+.header-nav a:hover {
+  box-shadow: 0 8px 12px 0 rgba(2, 2, 2, 0.574);
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  transition: 0.5s ease;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <MainDiv />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import MainDiv from "@/components/MainDiv.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    MainDiv,
   },
 };
 </script>

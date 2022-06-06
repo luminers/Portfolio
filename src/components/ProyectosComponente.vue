3785417d3ed4bfdb592fc4e44e6365083e2409af
Joaquin Nuñez <magnanimoysimple@gmail.com>
2022-06-06 20:29:36 -0300

    Data de proyectos

--- src/components/ProyectosComponente.vue
+++ src/components/ProyectosComponente.vue
@@ -23,6 +23,7 @@ export default {
           link: "https://luminers.github.io/KainotoLandingPage/",
           img: "https://luminers.github.io/KainotoLandingPage/assets/img/nosotros.jpg",
           logo: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
             "https://www.returngis.net/wp-content/uploads/2012/05/logo_CSS3.png",
             "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
           ],
@@ -31,8 +32,32 @@ export default {
         {
           link: "https://luminers.github.io/WP-Api-SPA/",
           img: "https://css-tricks.com/wp-content/uploads/2022/05/wp-patterns-featured-image.jpg",
-          logo: [1, 2],
+          logo: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+            "https://www.returngis.net/wp-content/uploads/2012/05/logo_CSS3.png",
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+          ],
           title: "Scrapper con WordPress API",
+        },
+        {
+          link: "https://luminers.github.io/WP-Api-SPA/",
+          img: "https://thelemabogados.pe/wp-content/uploads/2021/04/ac-asesores-empresariales-DIAN-Tasa-inter%C3%A9s-moratorio-liquidar-deudas.jpg",
+          logo: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+            "https://www.returngis.net/wp-content/uploads/2012/05/logo_CSS3.png",
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+          ],
+          title: "Caluladora de intereses",
+        },
+        {
+          link: "https://luminers.github.io/WP-Api-SPA/",
+          img: "https://i.ytimg.com/vi/zKZrHr4yDZc/maxresdefault.jpg",
+          logo: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+            "https://blog.repl.it/images/kaboom/kaboom.png",
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+          ],
+          title: "Mario Bros",
         },
       ],
     };
@@ -45,7 +70,7 @@ export default {
 
 <style scoped>
 .main-section {
-  height: 100vh;
+  height: 200vh;
   display: flex;
   flex-flow: column wrap;
   align-items: center;

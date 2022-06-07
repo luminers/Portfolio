9919c4756761e2da27206ea0515014c5eb43367e
Joaquin Nuñez <magnanimoysimple@gmail.com>
2022-06-06 23:02:45 -0300

    Sobre mi

---
+++ src/components/Perfil.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="main-section">
+    <h3>Perfil</h3>
+    <div class="div-content perfil">
+      <p>
+        Soy un estudiante universitario en busca de una oportunidad de trabajo
+        que me permita crecer laboral y personalmente. Apasionado por la
+        tecnología y la innovación, pero dispuesto a aprender sobre cualquier
+        rubro. Cuento con una gran capacidad de aprendizaje y un gran sentido de
+        responsabilidad.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "ResumenComponente",
+};
+</script>
+
+<style scoped>
+.perfil {
+  width: 70%;
+  height: auto;
+}
+</style>
